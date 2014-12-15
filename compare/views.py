d9e8940e3cc693de996178d992e2f6dd75b7fdc7
Giovanni Yoko Kristianto <giovanni@Giovannis-MacBook-Pro.local>
2014-12-16 00:56:55 +0900

    add new files

--- compare/views.py
+++ compare/views.py
@@ -1,6 +1,11 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+from os import path, getcwd
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello world')
+    queries = [{'title':'FT-20', 'maths':[{'tex':'\sqrt{3}'}]}]
+    template = loader.get_template('index.html')
+    context = RequestContext(request, {'queries':queries,})
+    return HttpResponse(template.render(context))
